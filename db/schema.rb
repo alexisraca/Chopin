44f58bbb28a31c51cc4a1de9a189683f936fc4dc
Rolando <roga.zero@gmail.com>
2016-08-25 00:10:55 -0500

    Creado vista de lista de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819065710) do
+ActiveRecord::Schema.define(version: 20160824191200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "statement_id"
+    t.integer  "product_id"
+    t.integer  "quantity"
+    t.float    "total"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
@@ -22,6 +31,15 @@ ActiveRecord::Schema.define(version: 20160819065710) do
     t.date     "completed_at"
     t.float    "total"
     t.string   "state"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.float    "total"
+    t.date     "settled_at"
+    t.string   "state"
+    t.integer  "statement_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
@@ -34,6 +52,14 @@ ActiveRecord::Schema.define(version: 20160819065710) do
     t.string   "sku"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "statements", force: :cascade do |t|
+    t.integer  "order_id"
+    t.string   "state"
+    t.float    "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
