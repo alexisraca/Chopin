05ce35ffe7de5d2e84982abc2d8971cf65509ecc
Amed <amed157@hotmail.com>
2016-08-28 03:39:49 -0500

    panel de ordenes

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,44 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.all
+    @orders = Order.all.order(:created_at)
   end
+
+  def new
+    @order = Order.new
+  end
+  
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to admin_orders_path
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      flash[:notice] = "Creacion Existosa"
+      redirect_to admin_orders_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update_attributes(order_params)
+      redirect_to admin_orders_path
+    else
+      render :edit
+    end
+  end
+
+  def order_params
+    params.require(:order).
+      permit(:user_id, :comment, :completed_at, :total, :state)
+  end
+
 end
