539203e4a37182dc298d0bbc0b92acb88002bd52
alexisraca <alexisraca@hotmail.com>
2016-09-03 12:58:31 -0500

    estilos al modal de usuarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :lockable, authentication_keys: [:username]
 
-  validates :username, presence: { message: "No puede estar en blanco" }
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
   validates_confirmation_of :password
+
+  validates :first_name, :last_name, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
   has_many :orders
-  validates :first_name, :last_name, presence: { message: "No puede estar en blanco" }
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
 end
 
