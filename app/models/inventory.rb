9fd8737efd752f37e54edf30eb20940122e69638
alexisraca <alexisraca@hotmail.com>
2016-09-27 23:03:26 -0500

    fix productos new

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,8 +2,6 @@ class Inventory < ActiveRecord::Base
   belongs_to :variant
   belongs_to :branch
   has_one :product, through: :variant
-
-  validates :quantity, presence: true
 end
 #
 # variant_id
