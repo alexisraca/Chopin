52df9ecc5415fa27cdb2736619b20d00baded175
alexisraca <alexisraca@hotmail.com>
2016-10-06 00:34:18 -0500

    agregar formulario de busqueda de variants

--- app/controllers/sales/orders_controller.rb
+++ app/controllers/sales/orders_controller.rb
@@ -1,4 +1,8 @@
 class Sales::OrdersController < ApplicationController
   def new
+    @q = Variant.ransack(params[:q])
+    @variants = @q.result(distinct: true).
+                  paginate(page: params[:page], per_page: 30).
+                  order(:created_at)
   end
 end
--- app/controllers/sales/variants_controller.rb
+++ app/controllers/sales/variants_controller.rb
@@ -1,2 +1,8 @@
 class Sales::VariantsController < ApplicationController
+  def index
+    @q = Variant.ransack(params[:q])
+    @variants = @q.result(distinct: true).
+                  paginate(page: params[:page], per_page: 30).
+                  order(:created_at)
+  end
 end
