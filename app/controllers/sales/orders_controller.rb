dc184bb28da45da2800da8a36a4e96afbf1c6d8f
Benjamin <macidion@hotmail.com>
2016-10-11 01:13:45 -0500

    Se modifica order controller

--- app/controllers/sales/orders_controller.rb
+++ app/controllers/sales/orders_controller.rb
@@ -2,7 +2,18 @@ class Sales::OrdersController < ApplicationController
   def new
     @q = Variant.ransack(params[:q])
     @variants = @q.result(distinct: true).
-                  paginate(page: params[:page], per_page: 10).
-                  order(:created_at)
+                   paginate(page: params[:page], per_page: 10).
+                   order(:created_at)
+    @order = find_or_create_order
+  end
+  def find_or_create_order
+    order = Order.includes(statements: :line_items).
+                  where(line_items: { statement_id: nil  }).
+                  order("orders.created_at DESC").first
+    if !order 
+      order = Order.create(user: current_user, state: Order::SUMARY)
+      order.statements.create(total: 0.0) 
+    end
+    order
   end
 end
