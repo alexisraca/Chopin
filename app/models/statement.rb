183c79ead82e04a555ff3f42f129e0279a610784
USUARIO <rolando>
2016-10-14 01:21:43 -0500

    se agrega la funcion autorender de la venta y agregar items

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -2,24 +2,32 @@ class Statement < ActiveRecord::Base
   attr_accessor :user_id
   has_many :payments
   has_many :line_items
+  has_many :variants, through: :line_items
   validates :order_id, :total, presence: { message: "No puede estar en blanco" }
 
   belongs_to :order
 
   def add_line_item(variant, kuantity = 1)
-    new_line_item = line_items.build(
-                      variant: variant,
-                      quantity: kuantity,
-                      total: variant.price * kuantity
-                    )
+    new_line_item = nil
     begin
       LineItem.transaction do
-        raise ActiveRecord::RecordInvalid.new(new_line_item)
-        new_line_item.save!
+        if variants.include?(variant)
+          new_line_item = line_items.select { |li| li.variant == variant }.first
+          new_line_item.quantity += kuantity
+          new_line_item.save!
+        else
+          new_line_item = line_items.build(
+                                      variant: variant,
+                                      quantity: kuantity,
+                                      total: variant.price * kuantity
+                                    )
+          new_line_item.save!
+        end
+        #raise ActiveRecord::RecordInvalid.new(new_line_item)
       end
     rescue ActiveRecord::RecordInvalid => exception
       self.errors.add(:producto, message: "tuvo un error al agregarse")
     end
-    new_line_item
+    return new_line_item
   end
 end
