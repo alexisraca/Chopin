2431aa07a91eeb57fa7c7a7a7ae41a1fe6491ad0
alexisraca <alexisraca@hotmail.com>
2016-10-26 22:49:26 -0500

    imprimir usuarios en dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,10 @@
 class DashboardController < ApplicationController
 
   def index
-
+    @q = User.non_master.ransack(params[:q])
+    @users = @q.result(distinct: true).
+               paginate(page: params[:page], per_page: 30).
+               order(:created_at)
   end
 
   def show
